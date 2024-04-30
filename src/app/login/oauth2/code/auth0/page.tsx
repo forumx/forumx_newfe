e33e1b9f6b4384c70ad39b7fb137081958d6b41d
Huy Nguyen <huynguyenquang030803@gmail.com>
2024-05-01 01:15:13 +0700

    update

--- src/app/login/oauth2/code/auth0/page.tsx
+++ src/app/login/oauth2/code/auth0/page.tsx
@@ -1,12 +1,29 @@
+'use client'
+import axios from "axios";
+import { useSearchParams } from "next/navigation";
+import { useEffect } from "react";
 
 
 const Auth0Page = async(props) => {
-	
-	await console.log("check prop: ", props);
-	
+	const backend = 'http://localhost:8080';
+	 console.log("check prop: ", props);
+	 useEffect(() => {
+		axios.get(`${backend}/login/oauth2/code/auth0`, {
+			params: {
+				code: props.searchParams.code,
+				state: props.searchParams.state
+			},
+			withCredentials: true
+		}).then((res) => {
+			console.log("check res: ", res);
+		}).catch((error) => {
+			console.log("check error: ", error);
+		})
+	}, [])
+		
 	return (
 		<>
-			auth0 page
+		
 		</>
 	)
 }
--- src/components/redirectLoginPage.tsx
+++ src/components/redirectLoginPage.tsx
@@ -9,15 +9,23 @@ const RedirectLoginPage = ({ shouldRedirect }) => {
 	
 	useEffect(() => {
 		const getRedirectUrl = async () => {
-			axios.get(`${process.env.NEXT_PUBLIC_redirectLoginURL}`)
-				.then((res) => {
-					window.location.href = res.data.redirectUrl;
-					console.log(res);
-				})
-				.catch((err) => {
-					console.error(err);
-				})
+			try {
+				const response = await fetch(`${process.env.NEXT_PUBLIC_redirectLoginURL}`);
+				if (!response.ok) {
+					throw new Error('Network response was not ok');
+				}
+				const data = await response.json();
+				if (!data) {
+					throw new Error('Missing redirectURI in JSON response');
+				}
+				console.log("redirectURI: ", data.redirectUrl);
+				location.replace(data.redirectUrl);
+			} catch (error) {
+				console.error('Error fetching redirect URL:', error.message);
+				// Xử lý lỗi ở đây (ví dụ: hiển thị thông báo lỗi cho người dùng)
+			}
 		};
+		
 		
 		if (shouldRedirect) {
 			getRedirectUrl();
